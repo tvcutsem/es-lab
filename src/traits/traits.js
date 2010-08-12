109b9978f89f0b54780124f1a71d03282f367a4a
tvcutsem <tvcutsem@aeea10e6-ec39-11de-83d8-6d5adf01d787>
2010-08-12 07:54:35 +0000

    reverted earlier change due to bug in V8, which is now fixed, cf http://code.google.com/p/v8/issues/detail?id=809

--- src/traits/traits.js
+++ src/traits/traits.js
@@ -77,9 +77,7 @@ var Trait = (function(){
         for (var i = 0, len = arr.length; i < len; i++) { fun(arr[i]); }
       };
   
-  // on v8 version 2.3.4.1, Object.freeze(obj) returns undefined instead of obj
-  var freeze = (Object.freeze ? function(obj) { Object.freeze(obj); return obj; }
-                              : function(obj) { return obj; });
+  var freeze = Object.freeze || function(obj) { return obj; };
   var getPrototypeOf = Object.getPrototypeOf || function(obj) { 
     return Object.prototype;
   };
