491fd6f76466ae10b7693abd12f4250207419ac5
erights <erights@aeea10e6-ec39-11de-83d8-6d5adf01d787>
2012-01-09 05:51:40 +0000

    added missing "use strict";

--- src/ses/amdTest3.js
+++ src/ses/amdTest3.js
@@ -25,6 +25,8 @@
     define :
     function(A,F) { module.exports = F.apply(null, A.map(require)); }}).
 define([], function() {
+  "use strict";
+         
   return function() {
     return 'test';
   };
